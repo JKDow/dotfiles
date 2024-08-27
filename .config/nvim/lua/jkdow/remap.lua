ec4dac1dede7f50d696c8af3e8fbb9df66f7c257
JKDow <jk.dowling22@gmail.com>
2024-08-27 16:06:42 +1000

    Save PC changes

--- .config/nvim/lua/jkdow/remap.lua
+++ .config/nvim/lua/jkdow/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")    
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
@@ -57,3 +57,11 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+-- Unset the shift down binds 
+vim.api.nvim_set_keymap('n', '<S-Down>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<S-Down>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-Up>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<S-Up>', '<NOP>', { noremap = true, silent = true })
+
+
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,5 +1,5 @@
 # If you come from bash you might have to change your $PATH.
-export PATH=$HOME/.local/bin:/usr/local/bin:$PATH
+export PATH=$HOME/.local/bin:/usr/local/bin:$HOME/.config/composer/vendor/bin:$PATH
 
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
@@ -101,4 +101,12 @@ source $ZSH/oh-my-zsh.sh
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 source $HOME/.cargo/env 
 
+source $HOME/.zsh_profile
+
 bindkey -s ^f "tmux-sessionizer\n"
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
