72ad302bf1455385db3ce62c26311b09d1bb0002
JKDow <jk.dowling22@gmail.com>
2024-08-29 16:15:33 +1000

    Fix colorscheme and add laravel LSP

--- .config/nvim/lua/jkdow/plugins/colors.lua
+++ .config/nvim/lua/jkdow/plugins/colors.lua
@@ -1,29 +1,24 @@
-
-function ColorMyPencils(color)
-    -- color = color or "rose-pine"
-    -- color = color or "tokyonight"
-    color = color or "onedark"
-	vim.cmd.colorscheme(color)
-end 
-
 return {
+    {
+        'joshdick/onedark.vim',
+        name = "onedark",
+        lazy = false,
+        priority = 1000,
+        config = function()
+	        vim.cmd.colorscheme("onedark")
+            -- ColorMyPencils()
+        end
+    },
+
     {
         'rose-pine/neovim',
         name = 'rose-pine',
         config = function()
-            vim.cmd('colorscheme rose-pine')
+            -- vim.cmd('colorscheme rose-pine')
         end
     },
 
     {
         'folke/tokyonight.nvim'
-    },
-
-    {
-        'joshdick/onedark.vim',
-        name = "onedark",
-        config = function()
-            ColorMyPencils()
-        end
     }
 }
