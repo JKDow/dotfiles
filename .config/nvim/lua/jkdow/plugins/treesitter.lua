bcba05fca937f6a65acfb46598a9c4b4fcd49c62
JKDow <jk.dowling22@gmail.com>
2024-08-29 16:37:19 +1000

    Cleanup packer files

--- .config/nvim/lua/jkdow/plugins/treesitter.lua
+++ .config/nvim/lua/jkdow/plugins/treesitter.lua
@@ -1,11 +1,10 @@
-
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function() 
+    config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { 
-                "c", "lua", "vimdoc", "rust",
+            ensure_installed = {
+                "c", "lua", "vimdoc", "rust", "php",
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
@@ -21,13 +20,9 @@ return {
 
             highlight = {
                 enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
                 additional_vim_regex_highlighting = {"php", "markdown"},
             },
+
         })
     end
 }
