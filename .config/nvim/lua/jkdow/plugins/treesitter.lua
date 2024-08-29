912b3894ecedeb7c67c4d3602f613ed4066db6d0
JKDow <jk.dowling22@gmail.com>
2024-08-29 16:47:16 +1000

    Add blade parser to treesitter

--- .config/nvim/lua/jkdow/plugins/treesitter.lua
+++ .config/nvim/lua/jkdow/plugins/treesitter.lua
@@ -2,9 +2,20 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
+        -- setup parser for blade (PHP LARAVEL)
+        local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+        parser_config.blade = {
+            install_info = {
+                url = "https://github.com/EmranMR/tree-sitter-blade",
+                files = { "src/parser.c" },
+                branch = "main",
+            },
+            filetype = "blade"
+        }
+
         require("nvim-treesitter.configs").setup({
             ensure_installed = {
-                "c", "lua", "vimdoc", "rust", "php",
+                "c", "lua", "vimdoc", "rust", "php", "blade",
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
@@ -14,15 +25,18 @@ return {
             -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
             auto_install = true,
 
-            indent = {
-                enable = true
-            },
+            indent = { enable = true },
 
             highlight = {
                 enable = true,
-                additional_vim_regex_highlighting = {"php", "markdown"},
+                additional_vim_regex_highlighting = { "php", "markdown" },
             },
-
         })
+
+        vim.api.nvim_exec([[
+            augroup BladeFiletypeRelated
+                au BufNewFile,BufRead *.blade.php set ft=blade
+            augroup END
+        ]], false)
     end
 }
