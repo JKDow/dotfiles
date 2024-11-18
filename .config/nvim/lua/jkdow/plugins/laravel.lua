74fccaa7d6c8b88684ce53bb6b98542335343fc7
JKDow <jk.dowling22@gmail.com>
2024-11-18 15:25:27 +1100

    Remove plugins directory from repo

--- .config/nvim/lua/jkdow/plugins/laravel.lua
+++ .config/nvim/lua/jkdow/plugins/laravel.lua
@@ -1,5 +1,6 @@
 return {
     "adalessa/laravel.nvim",
+    version = "2.2.2",
     enable = false,
     dependencies = {
         "nvim-telescope/telescope.nvim",
@@ -14,20 +15,21 @@ return {
         { "<leader>lf", ":Telescope laravel resources<cr>" },
     },
     event = { "VeryLazy" },
-    opts = {
-        lsp_server = "intelephense",
-        features = {
-            null_ls = {
-                enable = true,
+    config = function()
+        require("laravel").setup({
+            lsp_server = "intelephense",
+            features = {
+                null_ls = {
+                    enable = true,
+                },
+                route_info = {
+                    enable = true,  --- to enable the laravel.nvim virtual text
+                    position = 'right', --- where to show the info (available options 'right', 'top')
+                    middlewares = true, --- wheather to show the middlewares section in the info
+                    method = true,  --- wheather to show the method section in the info
+                    uri = true      --- wheather to show the uri section in the info
+                },
             },
-            route_info = {
-                enable = true, --- to enable the laravel.nvim virtual text
-                position = 'right', --- where to show the info (available options 'right', 'top')
-                middlewares = true, --- wheather to show the middlewares section in the info
-                method = true, --- wheather to show the method section in the info
-                uri = true --- wheather to show the uri section in the info
-            },
-        },
-    },
-    config = true,
+        })
+    end,
 }
