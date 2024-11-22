ea1e6907bb9684d795ca9881ff1d617f9e9b2e24
Josh Dowling <jk.dowling22@gmail.com>
2024-11-23 00:39:03 +1100

    Remove rust from ensure installed

--- .config/nvim/lua/jkdow/plugins/lsp.lua
+++ .config/nvim/lua/jkdow/plugins/lsp.lua
@@ -37,7 +37,6 @@ return {
             automatic_installation = true,
             ensure_installed = {
                 "lua_ls",
-                "rust_analyzer",
                 "intelephense",
             },
             handlers = {
