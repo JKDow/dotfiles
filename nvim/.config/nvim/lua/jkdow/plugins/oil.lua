9f69484e301d90e65508ffb7ac762757c1fef1f9
Josh Dowling <jk.dowling22@gmail.com>
2025-10-28 11:43:43 +1100

    Add oil features

--- nvim/.config/nvim/lua/jkdow/plugins/oil.lua
+++ nvim/.config/nvim/lua/jkdow/plugins/oil.lua
@@ -1,31 +1,32 @@
 return {
     'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {
-        view_options = {
-            show_hidden = true,
-        },
-        keymaps = {
-            ["<C-p>"] = false,
-        }
-    },
-    keys = {
-        { "-",          "<cmd>Oil<CR>", desc = "Open parent directory (Oil)" },
-        { "<leader>pv", "<cmd>Oil<CR>", desc = "File explorer" },
-    },
-    -- Optional dependencies
+    lazy = false,
     dependencies = {
-        { "nvim-mini/mini.icons", opts = {} },
+        { "nvim-mini/mini.icons",            opts = {} },
+        { "benomahony/oil-git.nvim",         opts = {} },
+        { "JezerM/oil-lsp-diagnostics.nvim", opts = {} },
         {
-            dir = vim.fn.stdpath("config") .. "/lua/oil_header", -- if you drop it in your config
+            dir = vim.fn.stdpath("config") .. "/lua/oil_header",
             name = "oil-header",
             config = function()
                 require("oil_header").setup()
             end,
         }
     },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    lazy = false,
+    config = function()
+        require('oil').setup {
+            view_options = {
+                show_hidden = false,
+            },
+            keymaps = {
+                ["<C-p>"] = false,
+                ["g."] = false,
+                ["."] = { "actions.toggle_hidden", mode = "n" },
+            },
+            delete_to_trash = true,
+        }
+
+        vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory (Oil)" })
+        vim.keymap.set("n", "<leader>pv", "<cmd>Oil<CR>", { desc = "Open parent directory (Oil)" })
+    end,
 }
